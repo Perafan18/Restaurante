fca682dcf397e554527bbfb440996925254a87f4
Pedro Perafan Carrasco <pedroperafan18@gmail.com>
2014-10-09 21:59:03 -0500

    Cambio 1.2
    Agrege la linea de hola

--- inicio.cpp
+++ inicio.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
+    printf("Holaa");
     char nava[30] = "Hola ._. , no veas nada de lo de sensualona\n";
-printf("Hola %s!\n",nava);
-
-return 0;
+    printf("Hola %s!\n",nava);
+    system("aguantavaramorro");
 }
