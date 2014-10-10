df492c77862d5ef277d58b7fc276445f017b2c40
Pedro Perafan Carrasco <pedroperafan18@gmail.com>
2014-10-09 21:25:03 -0500

    Nuevo Commit
    Hola nava!

--- inicio.cpp
+++ inicio.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+    char nava = "Hola ._. , no veas nada de lo de sensualona\n";
 char nombre[40] = "chaparrita hermosa y bella y sensualona";
 printf("Hola %s!\n",nombre);
 
