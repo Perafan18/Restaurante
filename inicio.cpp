c488d8085e2a38244df436b5c5ac3f3b22d6bcca
elnaveishon <kraker_910@hotmail.com>
2014-10-09 21:59:09 -0500

    Cambio 1.3 resturante
    Añadida funcion de menu principal

--- inicio.cpp
+++ inicio.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main(){
+	int opc;
     char nava[30] = "Hola ._. , no veas nada de lo de sensualona\n";
 printf("Hola %s!\n",nava);
-
+opc=menu();
 return 0;
+}
+
+void menu(){
+	int opc;
+	printf("Holi elije opcion\n\n1.-Holi\n\nHoli2\n");
+	scanf("%d",opc);
 }
