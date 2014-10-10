51550d5fd8f2ad6cce16127f1179c16c21f4893b
Pedro Perafan Carrasco <pedroperafan18@gmail.com>
2014-10-09 21:53:18 -0500

    Cambio 1.0 de restaurante
    El cambio que hice fue de la linea 30 a la 43 donde agrego jdjsjfs

--- inicio.cpp
+++ inicio.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    char nava = "Hola ._. , no veas nada de lo de sensualona\n";
-char nombre[40] = "chaparrita hermosa y bella y sensualona";
-printf("Hola %s!\n",nombre);
+    char nava[30] = "Hola ._. , no veas nada de lo de sensualona\n";
+printf("Hola %s!\n",nava);
 
 return 0;
 }
